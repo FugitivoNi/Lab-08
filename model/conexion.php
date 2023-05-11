fb2146b5855a485e838b7c7cc55d7a7581c6e374
FugitivoNi <jeffersonzulasmas@gmail.com>
2023-05-11 16:18:15 -0500

    conexion

--- model/conexion.php
+++ model/conexion.php
@@ -1,14 +1,9 @@
 <?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "crud";
+
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
+		'mysql://root:5y2vOH7Eocg1zdxP51x4@containers-us-west-49.railway.app:5531/railway',
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 	);
 } catch (Exception $e) {
